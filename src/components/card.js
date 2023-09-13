842d0da451bbac7a847198b6828f35cf746eee9e
imanlicina <licinaiman@gmail.com>
2023-09-13 18:14:18 +0200

    Pocetak

---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div style={{ width: "30vw", height: "30vh", backgroundColor: "pink" }}>
+      <h1>Iman Licins</h1>
+      <p>064 894 21 36</p>
+      <p>AvnojaBb</p>
+      <p>Sta radite?</p>
+    </div>
+  );
+}
+export default Card;
